b51b102e2da322d2288dfb8acda11d9dba8c6335
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-27 21:58:49 +0530

    comiit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      <section class="h-txt">
+      <h1>
+        <i><span>Increase</span> Your <br />Muscle <span>power</span></i>
+      </h1>
+      <p><i>“When you hit failure, your workout has just begun”</i></p>
+      <a href="#">Start Membership</a>
+    </section>
+    </>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+      <nav>
+        <div class="logo">
+          <i><b>BFC</b></i>
+        </div>
+        <div class="navbar">
+          <a href="#">Home</a>
+          <a href="#">Features</a>
+          <a href="#">Trainers</a>
+          <a href="#">Contact</a>
+        </div>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default Navbar
