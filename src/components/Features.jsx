879a23f630583794c8347cb4630d4f62d07b0ca8
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-12-30 16:22:34 +0530

    commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import fimages1 from '../images/1.svg'
 
 const Features = () => {
   return (
     <>
-      
+      <div className="image-grid">
+        <img src="1.svg" alt="Image 1"/>
+        <img src="2.svg" alt="Image 2"/>
+        <img src="3.svg" alt="Image 3"/>
+      </div>
     </>
   )
 }
