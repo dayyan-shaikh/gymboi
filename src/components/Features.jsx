f5701793b3d7bca6ffce08202034a13b8fd2ba19
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-01-05 17:30:44 +0530

    commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import imgg1 from './images/img1.svg'; 
+import imgg2 from './images/img2.svg'; 
+import imgg3 from './images/img3.svg'; 
 
 const Features = () => {
   return (
     <>
-      <div className="image-grid">
-        <img src="1.svg" alt="Image 1"/>
-        <img src="2.svg" alt="Image 2"/>
-        <img src="3.svg" alt="Image 3"/>
-      </div>
+    <div className='image-grid '>
+     <img src={imgg1} alt="img1" />
+     <img src={imgg2} alt="img1" />
+     <img src={imgg3} alt="img1" />
+     </div>
     </>
   )
 }
