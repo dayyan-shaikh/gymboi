3bb54e2fc52ddef1626c3d7cef388b6de91a262a
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-01-06 10:23:22 +0530

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className='contact'>
-        <form action="#" method="post">
-            <h3>Contact Us</h3>
-            <input type="text" id='name' placeholder='Name' />
-            <input type="text" id='email' placeholder='Email' />
-            <input type="text" id='Phone' placeholder='Phone No'/>
-            <textarea name="" id="message" rows="4" placeholder='Your Meassage'></textarea>
-            <button type='submit'>Submit</button>            
-        </form>
-    </div>
+    <footer>
+      <div className='contact'>
+          <form action="#" method="post">
+              <h3>Contact Us</h3>
+              <input type="text" id='name' placeholder='Name' />
+              <input type="text" id='email' placeholder='Email' />
+              <input type="text" id='Phone' placeholder='Phone No'/>
+              <textarea name="" id="message" rows="4" placeholder='Your Meassage'></textarea>
+              <button type='submit'>Submit</button>            
+          </form>
+      </div>
+    </footer>
   )
 }
 
